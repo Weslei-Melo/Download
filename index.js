e50d85c4a9eae617f34241e79b6478794c491dcd
Weslei-Melo <weslei_carlos13@hotmail.com>
2020-06-30 19:22:24 -0300

    Atualizaçoes

--- index.js
+++ index.js
@@ -5,135 +5,152 @@ const pdf = require('pdf-parse');
 
 const downloadPath = path.resolve(__dirname);
 
-async function downloadPdf() {
-  const options = {
-    hostname: "dejt.jt.jus.br",
-    port: 443,
-    path: "/cadernos/Diario_J_15.pdf",
-    method: "GET",
-    rejectUnauthorized: false,
-  };
+function downloadPdf() {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "dejt.jt.jus.br",
+      port: 443,
+      path: "/cadernos/Diario_J_15.pdf",
+      method: "GET",
+      rejectUnauthorized: false,
+    };
 
-  let file = fs.createWriteStream(`${downloadPath}/test.pdf`);
-
-  try {
+    let file = fs.createWriteStream(`${downloadPath}/test.pdf`);
     const req = https.request(options, (res) => {
       console.log("statusCode:", res.statusCode);
       console.log("headers:", res.headers);
 
-      res.on("data", async (d) => {
-        //process.stdout.write(d);
-      });
       res.pipe(file);
+
+      res.on("end", () => {
+        resolve(true);
+      });
     });
 
-    req.on("error", (e) => {
-      console.error(e);
+    req.on("error", (err) => {
+      console.log(err);
+      reject(false);
     });
-
     req.end();
-
-  } catch (err) {
-    console.log(err);
-  }
+  });
 }
 
 async function readPdf() {
   let dataBuffer = fs.readFileSync(`${downloadPath}/test.pdf`);
   try {
-    await pdf(dataBuffer).then(async (data) => {
-      // fs.writeFile(`${downloadPath}/example.txt`, data.text, function (erro) {
-      //   if (erro) {
-      //     throw erro;
-      //   }
-      //   console.log("Arquivo salvo");
-      // });
-      let string = data.text;
-      let conteudo1_processo;
-      let conteudo2_processo;
-      let advogado="OAB: 58246/SP";
-      let posicao;
-      let posicao2;
-      let vetor_localiza_advogado=[];
-      let pos_advogado = 0;
-      var vetor_publicacoes=[];
+      await pdf(dataBuffer).then(async (data) => {
+          // //gerar arquivo .txt com os dados do pdf
+          // fs.writeFile(`${downloadPath}/example.txt`, data.text, function (erro) {
+          //   if (erro) {
+          //     throw erro;
+          //   }
+          //   console.log("Arquivo salvo");
+          // });
 
-      while ( pos_advogado != -1 ) {
-        pos_advogado = string.indexOf(advogado ,pos_advogado + 1 );
+          let string = data.text;
+          let conteudo1_processo;
+          let conteudo2_processo;
+          let advogado="OAB: 25027/SP";
+          let posicao;
+          let posicao2;
+          let pos_final_advogado=0;
+          let vetor_localiza_advogado=[];
+          let vetor_localiza_advogado_final=[];
+          let pos_advogado = 0;
+          var vetor_publicacoes=[];
 
-        if(pos_advogado!=-1){
-          vetor_localiza_advogado.push(pos_advogado);
-        }
-      }
-      // for (let i = 0; i < vetor_localiza_advogado.length; i++) {
-      //   console.log(vetor_localiza_advogado[i],"   ");
-      // }
-      console.log( vetor_localiza_advogado.length, "processo(s) publicado(s)");
+          // encntrar todos locais onde foi citado a variavel "advogado"
+          while ( pos_advogado != -1 ) {
+              pos_advogado = string.indexOf(advogado ,pos_advogado + 1 );
+              pos_final_advogado=string.indexOf("Processo Nº" ,pos_advogado + 1 );
 
-        for (var j = 0; j < vetor_localiza_advogado.length; j++) {
-          posicao=string.indexOf("Processo Nº",vetor_localiza_advogado[j]-500);
-          posicao2=vetor_localiza_advogado[j];
-         
-          if(posicao2 ===-1 || posicao2===-1){
-            break;
+              //vereficar se em um publicação(Processo) aparece a varial advogado mais de uma vez (repetidos)
+              if(pos_advogado!=-1){
+                  
+                  let repetido=0;
+
+                  for(let i=0; i< vetor_localiza_advogado_final.length ;i++){
+                      if(vetor_localiza_advogado_final[i] == pos_final_advogado){
+                          repetido=1;
+                          break;
+                      }
+                  }
+
+                  if(repetido == 0){
+                      vetor_localiza_advogado_final.push(pos_final_advogado);
+                      vetor_localiza_advogado.push(pos_advogado);
+                      // console.log("Posição 1: ",pos_advogado, "Posição 2: ",pos_final_advogado);
+                  }
+              }
           }
-          conteudo1_processo=string.substring(posicao,posicao2);
+
+            console.log("Tamanho do vetor_localiza_advogado: ",vetor_localiza_advogado.length);
+          for (var j = 0; j < vetor_localiza_advogado.length; j++) {
+              //preciso melhorar essa parte
+              posicao=string.indexOf("Processo Nº",vetor_localiza_advogado[j]-500);
+              posicao2=vetor_localiza_advogado[j];
+              
+              if(posicao ===-1 ){
+                  break;
+              }
+              conteudo1_processo=string.substring(posicao,posicao2);
+
+              posicao =vetor_localiza_advogado[j];
+              posicao2= string.indexOf("Processo Nº",posicao);
+              //pode ser o ultimo processo do pdf, precisa melhorar
+              if(posicao2===-1){
+                  posicao2= string.indexOf("SUMÁRIO",posicao);
+              }
+              if(posicao2 ===-1 || posicao2===-1){
+                  break;
+              }
+              
+              
+              conteudo2_processo=string.substring(posicao,posicao2-1);
+
+              vetor_publicacoes.push(conteudo1_processo.concat(conteudo2_processo));
           
-          // console.log("INICIO:",posicao," FIM: ",posicao2);
-          // console.log("Posição 1: ",posicao, "Posição 2: ",posicao2);
-          // console.log(conteudo_processo);
-          
-          posicao =vetor_localiza_advogado[j];
-          posicao2= string.indexOf("Processo Nº",posicao);
-          if(posicao2===-1){
-            posicao2= string.indexOf("SUMÁRIO",posicao);
           }
-          if(posicao2 ===-1 || posicao2===-1){
-            break;
-          }
+
+          //Quantas citações teve 
+          console.log( vetor_publicacoes.length, "processo(s) publicado(s)");
         
-          console.log("Posição 1: ",posicao, "Posição 2: ",posicao2);
-          conteudo2_processo=string.substring(posicao,posicao2-1);
-          let conteudo3_processo= conteudo1_processo.concat(conteudo2_processo);
 
-          vetor_publicacoes.push(conteudo3_processo);
+          var ano= new  Date();
+          ano=ano.getFullYear();
+          string_ano="de ".concat(ano);
+          console.log(string_ano);
 
-        }  
-
-        for(let i = 0; i < vetor_publicacoes.length; i++){
-          console.log("Publicação: ",i+1);
-         
-          
-          let x=vetor_publicacoes[i].indexOf("Código para aferir");
-          
-
-          if(x!=-1){
-            vetor_publicacoes[i]=Remove(vetor_publicacoes[i],x,173);
+          for(let i = 0; i < vetor_publicacoes.length; i++){
+              console.log("Publicação: ",i+1);
+              
+              //remove a parte do rodapé
+              let x=0;
+              let y =0;
+              //se uma publicação estiver em mais de 1 página
+              while(x!=-1){
+                  x=vetor_publicacoes[i].indexOf("Código para aferir autenticidade deste caderno",x);
+                  y=vetor_publicacoes[i].indexOf(string_ano,x);
+                  if(x!=-1){
+                      let str1=vetor_publicacoes[i].substring(0,x);
+                      let str2= vetor_publicacoes[i].substring(y+8);
+                      vetor_publicacoes[i]=str1.concat(str2);
+                  }
+              }
+              //coloca espaços entre palavras que estão unidas
+              vetor_publicacoes[i] = put_space(vetor_publicacoes[i]);
+              console.log(vetor_publicacoes[i]);
+              console.log("//////////////////////////////////////////////////////////////////////////////////////////////////////////");
           }
-          // Código para aferir autenticidade deste caderno: 152591
-          // 3000/2020
-          // Tribunal Regional do Trabalho da 15ª Região3
-          // Data da Disponibilização: Terça-feira, 23 de Junho de 2020
+          
 
-          vetor_publicacoes[i] = put_space(vetor_publicacoes[i]);
-
-           console.log(vetor_publicacoes[i]);
-
-          console.log("//////////////////////////////////////////////////////////////////////////////////////////////////////////");
-        }
-
-
-       
-
-    });
+      });
   } catch (err) {
     console.log(err);
   }
 }
 
-
-
-
+//Função para colocar espaços entre algumas palavras que ficaram juntas
 function put_space(string){
   let str= string;
   string=string.replace(eval('/'+"AUTOR"+'/g'),"AUTOR ");
@@ -149,11 +166,14 @@ function put_space(string){
   return (string);
 }
 
+//Função para remover a parte do rodapé da pagina quando um processo ocupada mais de uma pagina
 function Remove(str, startIndex, count) {
   return str.substr(0, startIndex) + str.substr(startIndex + count);
 }
 
 (async () => {
-//   //await downloadPdf();
-  await readPdf();
+  let verify = await downloadPdf();
+  if (verify) readPdf();
+  else console.log("Não conseguiu fazer o download do pdf");
+  readPdf();
 })();
